786c5bfd5309e6de8306f5e5f66dc4e0711bb6dd
Laura1385 <lauraproto13@gmail.com>
2023-04-07 00:28:53 +0200

    deleted modelpoint's file and added new model file for agglomerate clustering

--- utils.py
+++ utils.py
@@ -6,12 +6,12 @@ import plotly.express as px
 import sklearn
 import scipy as sp
 import warnings
-warnings.filterwarnings("ignore")
+warnings.filterwarnings('ignore')
 
 def border_msg(msg):
     row = len(msg)
     h = ''.join(['+'] + ['-' *row] + ['+'])
-    result= h + '\n'"|"+msg+"|" '\n' + h
+    result= h + '\n''|'+msg+'|' '\n' + h
     print(result)
 
 def analisys(filename):
@@ -22,15 +22,15 @@ def analisys(filename):
     border_msg('Informazioni DataFrame:')
     df.info()
 
-    print("\n - Numero di osservazioni (righe) x caratteristiche (colonne):", df.shape, "\n\n")
-    print(border_msg('Anteprima del DataFrame:'),df.head(5), "\n")
-    print(border_msg('Statistiche di base:'),df.describe(), "\n")
-    print(border_msg('Analisi variabili:'), df.nunique(), "\n\n")
+    print('\n - Numero di osservazioni (righe) x caratteristiche (colonne):', df.shape, '\n\n')
+    print(border_msg('Anteprima del DataFrame:'),df.head(5), '\n')
+    print(border_msg('Statistiche di base:'),' ',df.describe(), '\n')
+    print(border_msg('Analisi variabili:'),'\n', df.nunique(), '\n\n')
 
     dfname= str(filename) 
-    name= dfname[:-4]#take values before point
-    prefix= "df_"
-    f = print("Rinomino il DataFrame in:", prefix + name)
+    name= dfname[:-4]#take values before the point
+    prefix= 'df_'
+    f = print('Rinomino il DataFrame creato in:', prefix + name)
 
 def confusion_matrix(df):
     #Import data
@@ -41,26 +41,26 @@ def confusion_matrix(df):
     #print(data.corr())
 
     #Create space of the graphic 
-    fig, ax = plt.subplots(figsize=(9, 6))
+    fig, ax = plt.subplots(figsize=(7, 4))
 
     #Create the mask to NOT have graphic redundancy
     mask = np.triu(np.ones_like(corr, dtype=bool), k = 1)
 
     #Create correlation palette and heat map
-    cmap = sns.color_palette("Spectral_r", as_cmap=True)
+    cmap = sns.color_palette('Spectral_r', as_cmap=True)
     dataplot = sns.heatmap(corr, mask=mask, center=0, annot=True, annot_kws={'size': 8}, fmt='.1f', cmap=cmap, linewidths=.35)
-    plt.title(f"Correlation Matrix Animals", fontsize=15)
+    plt.title(f'Correlation Matrix Animals', fontsize=15)
     plt.show()
     
 def update_animal_name(df, old_name, new_name):
     index = df.index[df['animal_name (nome_animale)'].str.startswith(old_name)]
-    if len(index) > 0:
+    if len(index) > 0: #cerca il nome
         old_value = df.at[index[0], 'animal_name (nome_animale)']
         new_full_name = old_value.replace(old_name, new_name)
         df.at[index[0], 'animal_name (nome_animale)'] = new_full_name
-        print(f"Ho modificato '{old_value}' con '{new_full_name}'")
+        print(f"Ho modificato '{old_value}' con '{new_full_name}'' ")
     else:
-        print(f"Nessun animale trovato che inizi con '{old_name}'")
+        print(f"Nessun animale trovato che inizi con '{old_name}'' ")
 
 def modify_column(df, animal_column, column_to_modify, values_to_modify):
     for animal, value in values_to_modify.items():
