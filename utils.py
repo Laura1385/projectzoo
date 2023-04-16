1f647c5d6d251dc9be63297819ac2cdd36be695e
Laura1385 <lauraproto13@gmail.com>
2023-04-16 23:56:21 +0200

    Add Class and Pair confusion Matrix

--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
-import time
-import sys
+import os
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -11,30 +10,28 @@ import scipy as sp
 import warnings #only for os
 warnings.filterwarnings('ignore') #only for os
 from datetime import datetime
+from googletrans import Translator, constants
 from sklearn.manifold import TSNE
 from sklearn.cluster import KMeans, AgglomerativeClustering, DBSCAN
 from sklearn.metrics import silhouette_samples
 from sklearn.metrics import accuracy_score, classification_report
-from sklearn.metrics.cluster import pair_confusion_matrix
+from sklearn.metrics.cluster import pair_confusion_matrix, rand_score
 from sklearn.metrics import confusion_matrix  ###usato o no?
 
+
 def chose(csv):
+    filepath = '1.Input/' + csv
     if csv == 'class.csv':
-        df_class = pd.read_csv('class.csv')
+        df_class = pd.read_csv(filepath,sep=',')
         df_zoo = None # se csv è 'class.csv', impostiamo df_zoo a None
     elif csv == 'zoo.csv':
-        df_zoo = pd.read_csv('zoo.csv') # correggi l'errore qui
+        df_zoo = pd.read_csv(filepath,sep=',') 
         df_class = None # se csv è 'zoo.csv', impostiamo df_class a None
     else:
         df_class = None
         df_zoo = None
-            
-    choice = ''
-    if not sys.stdin.isatty():  # se script eseguito in modalità batch
-        time.sleep(5)
-    else:  #se script eseguito interattivamente
-        choice = input('Visualizzare l\'analisi(1) \ Salvare l\'analisi in un file.txt(2) \ Entrambe(3)?')
-    
+        
+    choice = input('Visualizzare l\'analisi(1) \ Salvare l\'analisi in un file.txt(2) \ Entrambe(3)?')
     if choice == '1' :
         print('\nEcco l\'analisi: \n')
         analisys(csv)
@@ -46,7 +43,9 @@ def chose(csv):
         analisys_txt(csv)
     else:
         print('\nHai scelto di non fare nulla\n')
+        
     return df_class, df_zoo
+    
 
 def border_msg(msg):
     row = len(msg)
@@ -55,7 +54,8 @@ def border_msg(msg):
     print(result)
 
 def analisys(file_scan):
-    df = pd.read_csv(file_scan,sep=',')
+    filepath = '1.Input/' + file_scan
+    df = pd.read_csv(filepath,sep=',')
     border_msg('Nome File:')
     print("\n"+ file_scan, "\n")
     border_msg('Informazioni DataFrame:')
@@ -66,13 +66,19 @@ def analisys(file_scan):
     print(border_msg('Analisi variabili:'),'\n', df.nunique(), '\n\n')
     
 def analisys_txt(file_scan):
-    df = pd.read_csv(file_scan,sep=',')
+    filepath = '1.Input/' + file_scan
+    df = pd.read_csv(filepath,sep=',')
     current_dateTime = datetime.now()
     day, month, year = current_dateTime.day, current_dateTime.month, current_dateTime.year
     time, hour, minute = current_dateTime.time, current_dateTime.hour, current_dateTime.minute
     
     namef ='Report_'+ file_scan[:-4] +'.txt'
-    with open(namef, "w") as f:
+    folder = '2.Report'
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    path = os.path.join(folder, namef)
+
+    with open(path, "w") as f:
         f.write(f"Report del {day}/{month}/{year} ore {hour}:{minute}")
         f.write('\n\nAnalisi del file:\n')
         f.write('\n'+ file_scan + '\n\n\n')
@@ -86,27 +92,55 @@ def analisys_txt(file_scan):
         f.write('Statistiche di base:\n' + str(df.describe()) + '\n\n')
         f.write('\n----------------------------------------------------------------------\n\n')
         f.write('Analisi variabili:\n' + str(df.nunique()) + '\n\n')   
-    print('\nIl file', namef, 'è stato creato nella directory del progetto')
+    print(f'\nIl file {namef} è stato creato nella cartella {folder}')
 
 def correlation_matrix(df):
     #Import data
     data = df
-    corr = data.corr() #osx need (numeric_only=True)
-    #Print data
+    corr = data.corr() #os need (numeric_only=True)
     #print(data.corr())
-    #Create space of the graphic 
-    fig, ax = plt.subplots(figsize=(7, 4))
+    fig, ax = plt.subplots(figsize=(8, 5))
     #Create the mask to NOT have graphic redundancy
     mask = np.triu(np.ones_like(corr, dtype=bool), k = 1)
     #Create correlation palette and heat map
     cmap = sns.color_palette('Spectral_r', as_cmap=True)
     dataplot = sns.heatmap(corr, mask=mask, center=0, annot=True, annot_kws={'size': 8}, fmt='.1f', cmap=cmap, linewidths=.35)
     plt.title('Correlation Matrix Animals', fontsize=15)
+    plt.savefig('3.Graphics/1.Correlation Matrix Animals.png')
     plt.show()
+    
+class TranslatorColumnRow:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.df = pd.read_csv(self.filepath, sep=',')
+
+        # init the Google translator
+        self.translator = Translator()
+
+        # create animal's name list
+        name_list = self.df['animal_name'].tolist()
+
+        for index, name in enumerate(name_list):
+            # use translator object for animal's name translate, with translate() method, assigned to the translation variable
+            translation = self.translator.translate(name, dest="it")
+            translation_lowercase = translation.text.lower()
+            # change the value of first column's dataframe, adding a string to the original animal name
+            self.df.at[index, 'animal_name'] = f"{name} ({translation_lowercase})"
+
+        # create empty list
+        new_columns = []
+
+        # translate column headers (the first row)
+        for col in self.df.columns:
+            translation = self.translator.translate(col, dest="it")
+            translation_lowercase = translation.text.lower()
+            # change the value of column names, adding a string to the original name
+            new_columns.append(f"{col} ({translation_lowercase})")
+        self.df.columns = new_columns
     
 def update_animal_name(df, old_name, new_name):
     index = df.index[df['animal_name (nome_animale)'].str.startswith(old_name)]
-    if len(index) > 0: #cerca il nome
+    if len(index) > 0: #search name
         old_value = df.at[index[0], 'animal_name (nome_animale)']
         new_full_name = old_value.replace(old_name, new_name)
         df.at[index[0], 'animal_name (nome_animale)'] = new_full_name
@@ -149,8 +183,6 @@ def km_clust(X, n_clusters, init, n_init=10, max_iter=300, tol=1e-4, random_stat
                     max_iter=max_iter, tol=tol, 
                     random_state=random_state)
     y_km_clustering = kmeans.fit_predict(X)+1 #correct labels(now start from 1, not 0)
-    
-    #Stampa le prime 10 istanze clusterizzate e le etichette univoche dei cluster.
     #print(f"Primi 10 punti clusterizzati: {y_km_clustering[:10]}")
     print(f"Etichette univoche dei cluster: {np.unique(y_km_clustering)}")
     return kmeans, y_km_clustering
@@ -193,7 +225,7 @@ def results_group(data, df, y, nome_algoritmo):
     #newdf with 2 coloums
     df_cl = pd.DataFrame(new_list, columns=['Animal', 'Pred Labels'])
 
-    #animal in every cluster
+    #animal in each cluster
     group_cl = df_cl.groupby(['Pred Labels'])
     print('Algoritmo', nome_algoritmo, '\n')
     for num, (nome, gruppo) in enumerate(group_cl):
@@ -205,9 +237,9 @@ def results_group(data, df, y, nome_algoritmo):
     return df_cl  
     
 def plot_silhouette(X, kmeans, title='Silhouette Plot'):
-    cluster_labels = np.unique(kmeans.labels_) #array of labels (ex kmplus_clustering)
+    cluster_labels = np.unique(kmeans.labels_) #array of labels
     n_clusters =  len(cluster_labels)
-    silhouette_vals = silhouette_samples(X, kmeans.labels_, metric='euclidean')#silhoutte for each sample (ex kmplus_clustering)
+    silhouette_vals = silhouette_samples(X, kmeans.labels_, metric='euclidean')#silhoutte for each sample
     y_ax_lower, y_ax_upper = 0, 0
     yticks = []
     
@@ -257,51 +289,92 @@ def merge(df, results, namecsv=''):
     df_pred['Pred Labels'] = df_pred['Pred Labels'].astype('int64')
     
     dfmerged = pd.merge(df_real, df_pred, on='Animal')
-    dfmerged_ord = dfmerged.sort_values('Real Labels') #VALORI ORDINATI
+    dfmerged_ord = dfmerged.sort_values('Real Labels') #sort value
+    
     namecsv = namecsv + '.csv'
-    dfmerged_ord.to_csv(namecsv, index=False)
-    print('Esportato file',namecsv,'nella directory del progetto')
+    namefolder = '4.Merge'
+    path = os.path.join(namefolder, namecsv)
+    dfmerged_ord.to_csv(path, index=False)
+    
+    print('Esportato file', namecsv,'nella cartella', namefolder,'del progetto')
     return dfmerged_ord
 
-def plot_res(dfmerge, title):
+def plot_res(dfmerge, title, ax=None):
     X_features = dfmerge.iloc[: , 0]
     y_real= dfmerge.iloc[ :, -2] #The Y Label REAL
     y_predicted= dfmerge.iloc[ :, -1] #The predicted Y-label
-
-    plt.figure(figsize=(17, 4))
-    plt.plot(X_features, y_real, color='blue',label='Real')
-    plt.plot(X_features, y_predicted, color='orange',label='Predict')
-    plt.xticks(rotation=90, fontsize=8) 
-    plt.title(title ,fontsize=18)
-    plt.xlabel('Animal', fontsize=14)
-    plt.ylabel('Cluster', fontsize=14)
+    
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(17, 3))
+        fig.set_size_inches(17,3)
+    else:
+        fig = ax.get_figure()
+     
+    ax.plot(X_features, y_real, color='blue',label='Real')
+    ax.plot(X_features, y_predicted, color='orange',label='Predict')
+    ax.tick_params(axis='x', rotation=90, labelsize=8)
+    ax.set_title(title, fontsize=18)
+    ax.set_xlabel('Animal', fontsize=10)
+    ax.set_ylabel('Cluster', fontsize=10)
     #add point for each x values
-    plt.scatter(X_features, y_real, color='blue')
-    plt.scatter(X_features, y_predicted, color='orange')
-    plt.legend(fontsize=17)
-    plt.show()
+    ax.scatter(X_features, y_real, color='blue')
+    ax.scatter(X_features, y_predicted, color='orange')
+    ax.legend(fontsize=17)
+    #plt.show()
     return y_real, y_predicted
 
 def accurancy(y_real, y_predicted):
     acc = accuracy_score(y_real, y_predicted)
     print("Accuratezza: {:.3f}".format(acc))
+    return acc
 
-    report=classification_report(y_real, y_predicted)
-    print(report)
+def pair_conf_matrix(y_real, y_predicted, name):
+    PCM = pair_confusion_matrix(y_real, y_predicted)
+    print(f'Pair confusion Matrix of {name}\n{PCM}\n')
+    RandIndex = rand_score(y_real, y_predicted)
+    TN = PCM[0][0]
+    FP = PCM[0][1]
+    FN = PCM[1][0]
+    TP = PCM[1][1]
+    Precision = TP/(TP+FP)
+    Recall = TP/(TP+FN)
+    F1 = 2*Precision*Recall/(Precision+Recall)
+    print(f'Rand index: {RandIndex:.3f}')
+    print(f'Precision: {Precision:.3f}')
+    print(f'Recall: {Recall:.3f}')
+    print(f'F1: {F1:.3f}')
 
-def pair_conf_matrix(y_real, y_predicted, title):
-    cm = pair_confusion_matrix(y_real, y_predicted)
-    print('Matrix pair confusion\n',cm)
-    
-    cm1 = pair_confusion_matrix(y_real, y_predicted)
-    cm2 = confusion_matrix(y_real, y_predicted)
-    fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(10, 5))
-    im1 = ax1.imshow(cm1, cmap='coolwarm')
-    ax1.set_title(title)
-    plt.colorbar(im1, ax=ax1)
+    fig, axs = plt.subplots(ncols=2, figsize=(10, 5))
 
-    im2 = ax2.imshow(cm2, cmap='bwr')
-    ax2.set_title('Confusion matrix')
-    plt.colorbar(im2, ax=ax2)
+    # Plot confusion matrix as a heatmap
+    im = axs[0].imshow(PCM, cmap='coolwarm')
+    axs[0].set_title(name)
+    plt.colorbar(im, ax=axs[0])
+
+    # Plot precision and recall as bar charts
+    data = {'Precision': Precision, 'Recall': Recall}
+    axs[1].bar(data.keys(), data.values())
+    axs[1].set_ylim(0, 1)
+    axs[1].set_title('Precision and Recall')
+
     plt.show()
+    
+'''
+def eval_pair_confusion_matrix(y_real, y_predicted, name):
+    print(f'Values Pair confusion Matrix of {name}')
+    PCM = pair_confusion_matrix(y_real, y_predicted)
+    RandIndex = rand_score(y_real, y_predicted)
+    TN = PCM[0][0]
+    FP = PCM[0][1]
+    FN = PCM[1][0]
+    TP = PCM[1][1]
+    Precision = TP/(TP+FP)
+    Recall = TP/(TP+FN)
+    F1 = 2*Precision*Recall/(Precision+Recall)
+    print(f'Pair confusion matrix: \n {PCM}')
+    print(f'Rand index: {RandIndex}')
+    print(f'Precision: {Precision}')
+    print(f'Recall: {Recall}')
+    print(f'F1: {F1}')
+'''
     
